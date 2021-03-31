c2d771cc9413ee21dda993684c6a5185c15274f8
Nico <nico.nap@outlook.com>
2021-03-31 14:45:39 +0200

    PHP pagina maken van index.html

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link href="./style.css" rel="stylesheet" type="text/css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tim's Art</title>
+</head>
+<body>
+    <?php 
+        # Zorgt voor het maken van de navigatie
+        echo '<nav>
+            <p>Tim\'s Art</p>
+            <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#ourcollection">Shop</a></li>
+            <li><a href="./about.html">Over</a></li>
+            <li><a href="#">More</a></li>
+            </ul>
+            <img alt="Shopping cart" src="shoppingcart.svg" />
+        </nav>';
+
+        # Zorgt voor het maken van de landing image
+        echo '<div id="landingtext">
+            <h1 id="title">TIM SMELIK</h1>
+            <h3>Epoxy Kunstenaar</h3>
+            <a href="#ourcollection" id="shopnow">Shop Nu</a>
+        </div>';
+
+        # Zorgt voor het maken van de titel van de shop
+        echo '<div id="ourcollection">
+            <h3>ONZE COLLECTIE</h3>
+            <p>De collectie van Tim Smelik bestaat uit zijn eigen ideëen en creativiteit.</p>
+        </div>';
+
+        echo '<div id="shop">';
+        class Shopitem{
+            public $itemimg;
+            public $itemdescription;
+            public $itemprice;
+
+            public function getItem() {
+                echo '<div class="item">';
+                echo '<div id="'. $this->itemimg .'" class="itemimg"></div>';
+                echo '<span class="itemdescription">'. $this->itemdescription .'</span>';
+                echo '<span class="itemprice">'. $this->itemprice .'</span>';
+                echo '<a>Zie details</a>';
+                echo '</div>';
+            }
+
+            public function __construct($itemimg, $itemdescription, $itemprice) {
+                $this->itemimg = $itemimg;
+                $this->itemdescription = $itemdescription;
+                $this->itemprice = $itemprice;
+            }
+        }
+
+        $array = array(
+            "dancer" => array("img" => "dancer",
+            "description" => "Danser",
+            "price" => "€50,00",),
+            "face" => array("img" => "face",
+            "description" => "Oranje-rood Hoofd",
+            "price" => "€50,00",),
+            "purplehead" => array("img" => "purplehead",
+            "description" => "Paars Hoofd",
+            "price" => "€50,00",),
+        );
+
+        $firstItem = new Shopitem($array["dancer"]["img"], $array["dancer"]["description"], $array["dancer"]["price"]);
+        echo $firstItem->getItem();
+        $secondItem = new Shopitem($array["face"]["img"], $array["face"]["description"], $array["face"]["price"]);
+        echo $secondItem->getItem();
+        $thirdItem = new Shopitem($array["purplehead"]["img"], $array["purplehead"]["description"], $array["purplehead"]["price"]);
+        echo $thirdItem->getItem();
+        echo '</div>'
+    ?>
+</body>
+</html>
