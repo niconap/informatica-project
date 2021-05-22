064e7a785a3d00b6f9b5ded438e5964155ba2a28
GhaloGhertje <koenvdh7@gmail.com>
2021-05-22 20:11:28 +0200

    Update dbconnectie.php

--- core/dbconnectie.php
+++ core/dbconnectie.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = new PDO("sqlite:./core/database.sqlite", '', '', array(PDO::ATTR_PERSISTENT => TRUE));
+$db = new PDO("sqlite:../core/database.sqlite", '', '', array(PDO::ATTR_PERSISTENT => TRUE));
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
