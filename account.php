d9460d8b467c573b58534f6a2a48b0b8398a7abb
Nico <nico.nap@outlook.com>
2021-05-26 13:16:43 +0200

    Account pagina responsive maken

--- account.php
+++ account.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>Tim's Art - Account</title>
 		<link href="./css/style.css" rel="stylesheet" type="text/css">
-		<link href="./css/account.css?v=2" rel="stylesheet" type="text/css">
+		<link href="./css/account.css" rel="stylesheet" type="text/css">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<body>
